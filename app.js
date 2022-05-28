33dd18d24ba6e542a9a779212336d716a40d2605
gaeundev <gaeundev@gmail.com>
2022-05-29 01:41:00 +0900

    [feature] chat

---
+++ app.js
@@ -0,0 +1,25 @@
+import express from 'express';
+import http from 'http';
+import { Server } from 'socket.io';
+
+const app = express();
+const server = http.createServer(app);
+
+const io = new Server(server);
+
+app.get('/', (req, res) => {
+    res.sendFile(__dirname + '/index.html');
+});
+
+io.on('connection', (socket) => {
+    console.log('user connected');
+
+    socket.on('chat message', (msg) => {
+        io.emit('chat message', msg);
+    });
+});
+
+server.listen(3000, () => {
+    console.log('listening on *:3000');
+    console.log('http://localhost:3000/');
+});
